a70fe7fdf1f793e5d413c3d843d6725cc9b391a1
RahulPratap90 <rahulpratap601@gmail.com>
2024-09-28 10:25:53 +0530

    email vailidation update

--- Email_Validation.py
+++ Email_Validation.py
@@ -18,7 +18,7 @@ if len(email)>=6:
                         d=1
 
                 if k==1 or j==1 or d==1:
-                    print("Wrong email 5")
+                    print("Wrong email 5..")
                 else:
                     print("Right Email")
             else:
